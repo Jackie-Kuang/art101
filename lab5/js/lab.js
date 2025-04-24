c048eea2f8cdea43fabddef82fb506145da6d296
Jackie K <102831820+Jackie-Kuang@users.noreply.github.com>
2025-04-24 11:08:18 -0700

    lab 5 js, html, and css
    defined variables for my car and added output for that into html

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,49 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+* index.js - Lab 5: Data Types and Variables
+* Implementing JavaScript working with variables
+* Author: Jackie Kuang jhkuang@ucsc.edu
+* Date: 4/21/2025
+*/
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
 
-// let's get this party started
-main();
+// // let's get this party started
+// main();
+
+// Define Variables
+let carName = "Toyota";
+console.log("Make: " + carName);
+
+let carModel = "GR86";
+console.log("Model: " + carModel);
+
+let carColor = "Neptune Blue";
+console.log("Color: " + carColor);
+
+let carYear = 2023;
+console.log("Year: " + carYear);
+
+let currentYear = 2025;
+
+let carAge = currentYear - carYear;
+console.log ("Age: ", carAge);
+
+// Output
+document.writeln("Make: " + carName + "<br>");
+document.writeln("Model: " + carModel + "<br>");
+document.writeln("Color: " + carColor + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + carAge +  "<br>");
